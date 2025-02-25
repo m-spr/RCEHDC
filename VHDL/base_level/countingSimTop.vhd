9067e649326dff2fa3ced9e3f48b42f53ac14f37
m-spr <53905453+m-spr@users.noreply.github.com>
2025-02-25 18:44:17 +0100

    Update countingSimTop.vhd

--- VHDL/base_level/countingSimTop.vhd
+++ VHDL/base_level/countingSimTop.vhd
@@ -1,6 +1,30 @@
+-- MIT License
+
+-- Copyright (c) 2024 m-spr
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+ 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
+
+USE std.textio.ALL;
 
 ENTITY countingSimTop  IS
 	GENERIC (n : INTEGER := 10;		 --; 	-- bit-widths of memory pointer, counter and etc,,, 
@@ -30,6 +54,7 @@ component countingSim  IS
 		reg1Update, reg1rst, reg2Update, reg2rst   	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
 		muxSel   	 	: IN  STD_LOGIC_VECTOR (logInNum DOWNTO 0);
 		hv        		: IN  STD_LOGIC_VECTOR(d -1 DOWNTO 0);
+		CHV        		: IN  STD_LOGIC_VECTOR(d -1 DOWNTO 0);
 		pointer		 	: IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);	
 		dout	 		: OUT  STD_LOGIC_VECTOR(n+logInNum-1 DOWNTO 0)  	
 	);
@@ -46,7 +71,7 @@ component SeqAdderCtrl IS
 		muxSel 					: OUT STD_LOGIC_VECTOR(ceilingLogPop DOWNTO 0)
 	);
 end component;
-
+ 
 component countingSimCtrl IS
 	GENERIC (n : INTEGER := 10 ); --- bit pointer to memory
 	PORT (
@@ -57,38 +82,76 @@ component countingSimCtrl IS
 	);
 end component;
 
+--new memory signals
+type CHV_memory is array (classNumber-1 downto 0) of std_logic_vector(1023 downto 0);
+type CHV_memory_tosim is array (classNumber-1 downto 0) of std_logic_vector(d-1 downto 0);
+signal CHV_TO_OUT : CHV_memory_tosim;
+
+signal CHV : CHV_memory;
+file CHV_file : text open read_mode is "CHV_img.mif"; -- Specify your file name
+
 SIGNAL dones, runOut	: STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
 SIGNAL reg1Update, reg1rst, reg2Update, reg2rst	: STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
 SIGNAL muxSel	:  STD_LOGIC_VECTOR (logInNum DOWNTO 0);
 SIGNAL point	: STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+
+attribute MARK_DEBUG : string;
+attribute MARK_DEBUG of CHV : signal is "TRUE";
+attribute MARK_DEBUG of CHV_TO_OUT : signal is "TRUE";
+
 begin
+
+    process
+	variable mif_line : line;
+	variable temp_bv : bit_vector(1023 downto 0); -- Temporary buffer for each line
+    begin
+        -- Loop through each line of the file
+        for i in 0 to classNumber-1 loop
+            if not endfile(CHV_file) then
+                -- Read one line from the file
+                readline(CHV_file, mif_line);
+                -- Read the binary data into the temporary bit_vector
+                read(mif_line, temp_bv);
+                -- Convert the bit_vector to std_logic_vector and store it in the memory signal
+                CHV(i) <= to_stdlogicvector(temp_bv);
+            else
+                -- Handle end of file if fewer lines exist than expected
+                CHV(i) <= (others => '0'); -- Optional: Initialize remaining entries to 0
+            end if;
+        end loop;
+        wait; -- Stop the process after reading the file
+    end process; 
+
+    concatECC: FOR I IN classNumber-1 DOWNTO 0 GENERATE
+        classesECC: FOR k IN d-1 DOWNTO 0 GENERATE
+            CHV_TO_OUT(I)(k) <= CHV(I)(to_integer(unsigned(point)) + (2**n) * K);
+        END GENERATE classesECC;
+    END GENERATE concatECC;
+
 	AdderCtrl : SeqAdderCtrl
 	GENERIC MAP(logInNum, 
 			d )
 	PORT MAP(
-		clk, rst,
-		dones,
-		reg1Update, reg1rst,
-		reg2Update, reg2rst,
-		muxSel
-	);	
-		
-
+            clk, rst,
+            dones,
+            reg1Update, reg1rst,
+            reg2Update, reg2rst,
+            muxSel
+        );	
+    
 	countSimArr: FOR I IN classNumber-1 DOWNTO 0 GENERATE
 		comp : countingSim 
 		GENERIC MAP(n ,d, z, I, logInNum)
 		PORT MAP(
-			clk, rst, runOut, dones,
+			clk, rst, runOut, dones, 
 			reg1Update, reg1rst, reg2Update, reg2rst,
 			muxSel,
-			hv,
+			hv, CHV_TO_OUT(I),
 			point,
-			dout	(((I+1)*(n+logInNum))- 1 DOWNTO ((I)*(n+logInNum)))
+			dout(((I+1)*(n+logInNum))- 1 DOWNTO ((I)*(n+logInNum)))
 		);
 	END GENERATE countSimArr;
-	
-	
-	
+		
 	CompCtrl : countingSimCtrl
 	GENERIC MAP(n)
 	PORT MAp(
@@ -97,6 +160,7 @@ begin
 		runOut, dones, 
 		point
 	);
+	
 	pointer <= point;
 	done <= reg2Update;
 	
