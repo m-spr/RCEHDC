6fe335f9889d01233b0324e52e7768f3e3fcb215
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-17 18:14:20 +0100

    Update comparator.vhd

--- VHDL/lfsr/comparator.vhd
+++ VHDL/lfsr/comparator.vhd
@@ -24,15 +24,17 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY comparator  IS
-	GENERIC (len : INTEGER := 8);   -- bit width out adder
-	PORT (
-		a, b         : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		gr        : OUT  STD_LOGIC      --- case of a >= b gr ='1' other wise 0
-	);
-END ENTITY comparator ;
+ENTITY comparator IS
+    GENERIC (
+        len : INTEGER := 8  -- Bit width of the adder output
+    );
+    PORT (
+        a, b : IN  STD_LOGIC_VECTOR(len - 1 DOWNTO 0);
+        gr   : OUT STD_LOGIC  -- '1' when a >= b, otherwise '0'
+    );
+END ENTITY comparator;
 
-ARCHITECTURE behavioral OF comparator  IS
+ARCHITECTURE behavioral OF comparator IS
 BEGIN
-	gr <= '1' WHEN a > b ELSE '0';    --condition != STD_LOGIC_VECTOR (UNSIGNED(0)) ELSE '0';
+    gr <= '1' WHEN UNSIGNED(a) >= UNSIGNED(b) ELSE '0';  
 END ARCHITECTURE behavioral;
