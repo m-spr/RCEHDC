91e0a2ff72afee71e7365bdea5c3aea75ced7bfb
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-17 18:33:15 +0100

    Update recMux.vhd

--- VHDL/lfsr/recMux.vhd
+++ VHDL/lfsr/recMux.vhd
@@ -25,91 +25,116 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY mux2 IS
-	GENERIC (lenPop : INTEGER := 8);   -- bit width out popCounters
-	PORT (
-		sel 		: IN STD_LOGIC;
-		din1, din2  : IN  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
-		dout        : OUT  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
-	);
+    GENERIC (
+        lenPop : INTEGER := 8  -- Bit width of the output popCounters
+    );
+    PORT (
+        sel     : IN  STD_LOGIC;
+        din1    : IN  STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+        din2    : IN  STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+        dout    : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+    );
 END ENTITY mux2;
 
 ARCHITECTURE behavioral OF mux2 IS
-
 BEGIN
-
-	dout <= din1 WHEN (sel = '0') ELSE
-			din2 ;
-	
+    dout <= din1 WHEN (sel = '0') ELSE din2;
 END ARCHITECTURE behavioral;
+
 -------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY recMux IS
-	GENERIC ( n : INTEGER := 3;			-- MUX sel bit width (number of layer)
-			  lenPop : INTEGER := 8);   -- bit width out popCounters
-	PORT (
-		sel 		: IN STD_LOGIC_VECTOR (n DOWNTO 0);       -- sel<= sel && '0'
-		din			: IN  STD_LOGIC_VECTOR (((2**n)*lenPop)- 1 DOWNTO 0);
-		dout        : OUT  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
-	);
+    GENERIC (
+        n      : INTEGER := 3;   -- MUX select bit width (number of layers)
+        lenPop : INTEGER := 8    -- Bit width of output popCounters
+    );
+    PORT (
+        sel     : IN  STD_LOGIC_VECTOR(n DOWNTO 0);  
+        din     : IN  STD_LOGIC_VECTOR((2**n * lenPop) - 1 DOWNTO 0);
+        dout    : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+    );
 END ENTITY recMux;
 
 ARCHITECTURE behavioral OF recMux IS
 
-	COMPONENT mux2 IS
-		GENERIC (lenPop : INTEGER := 8);   -- bit width out popCounters
-		PORT (
-			sel 		: IN STD_LOGIC;
-			din1, din2  : IN  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
-			dout        : OUT  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
-		);
-	END COMPONENT;
-	
-	COMPONENT recMux IS
-		GENERIC ( n : INTEGER := 3;			-- MUX sel bit width (number of layer)
-				  lenPop : INTEGER := 8);   -- bit width out popCounters
-		PORT (
-			sel 		: IN STD_LOGIC_VECTOR (n DOWNTO 0);
-			din			: IN  STD_LOGIC_VECTOR (((2**n)*lenPop)- 1 DOWNTO 0);
-			dout        : OUT  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
-		);
-	END COMPONENT;
-	SIGNAL mux1Out, mux2Out : STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
-	
+    -- Component Declarations
+    COMPONENT mux2 IS
+        GENERIC (
+            lenPop : INTEGER := 8  -- Bit width of output popCounters
+        );
+        PORT (
+            sel     : IN  STD_LOGIC;
+            din1    : IN  STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+            din2    : IN  STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+            dout    : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT recMux IS
+        GENERIC (
+            n      : INTEGER := 3;   -- MUX select bit width (number of layers)
+            lenPop : INTEGER := 8    -- Bit width of output popCounters
+        );
+        PORT (
+            sel     : IN  STD_LOGIC_VECTOR(n DOWNTO 0);
+            din     : IN  STD_LOGIC_VECTOR((2**n * lenPop) - 1 DOWNTO 0);
+            dout    : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    -- Internal Signals
+    SIGNAL mux1Out, mux2Out : STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+
 BEGIN
 
-	last : if n = 1 generate
-		mux0 :	 mux2
-		GENERIC MAP(lenPop)
-		PORT MAP(
-			sel (0), din((2*lenPop)- 1 DOWNTO lenPop), din(lenPop-1 DOWNTO 0),
-			dout   
-		);	
-	end generate last;
-	internals : if n > 1 generate
-		recMux1	:	recMux 
-		GENERIC MAP( n-1, lenPop )
-		PORT MAP(
-			sel (n-1 DOWNTO 0),
-			din	(((2**n)*lenPop)- 1 DOWNTO ((2**(n-1))*lenPop)),
-			mux1Out
-		);
-		recMux2	:	recMux 
-		GENERIC MAP( n-1, lenPop )
-		PORT MAP(
-			sel (n-1 DOWNTO 0),
-			din	(((2**(n-1))*lenPop)- 1 DOWNTO 0),
-			mux2Out
-		);
-		
-		mux2l : mux2
-		GENERIC MAP(lenPop)
-		PORT MAP(
-			sel (n-1), mux1Out, mux2Out,
-			dout  
-		);
-	end generate internals;
-		
+    -- Base Case: Simple 2-to-1 MUX when `n = 1`
+    last: IF n = 1 GENERATE
+        mux0: mux2
+            GENERIC MAP (lenPop => lenPop)
+            PORT MAP (
+                sel  => sel(0),
+                din1 => din((2 * lenPop) - 1 DOWNTO lenPop),
+                din2 => din(lenPop - 1 DOWNTO 0),
+                dout => dout
+            );  
+    END GENERATE last;
+
+    -- Recursive Case: Divide and Conquer Multiplexing
+    internals: IF n > 1 GENERATE
+        recMux1: recMux
+            GENERIC MAP (
+                n      => n - 1,
+                lenPop => lenPop
+            )
+            PORT MAP (
+                sel  => sel(n-1 DOWNTO 0),
+                din  => din((2**n * lenPop) - 1 DOWNTO ((2**(n-1)) * lenPop)),
+                dout => mux1Out
+            );
+
+        recMux2: recMux
+            GENERIC MAP (
+                n      => n - 1,
+                lenPop => lenPop
+            )
+            PORT MAP (
+                sel  => sel(n-1 DOWNTO 0),
+                din  => din(((2**(n-1)) * lenPop) - 1 DOWNTO 0),
+                dout => mux2Out
+            );
+
+        -- Top-level 2-to-1 MUX
+        mux2l: mux2
+            GENERIC MAP (lenPop => lenPop)
+            PORT MAP (
+                sel  => sel(n-1),
+                din1 => mux1Out,
+                din2 => mux2Out,
+                dout => dout
+            );
+    END GENERATE internals;
+
 END ARCHITECTURE behavioral;
