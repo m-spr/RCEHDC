1a95e4b890d41fd5095c2131a6a8b839218659c6
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-17 14:32:49 +0100

    Update countingSim.vhd

--- VHDL/base_level/countingSim.vhd
+++ VHDL/base_level/countingSim.vhd
@@ -19,152 +19,78 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
- 
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
-USE std.textio.ALL;
-
-ENTITY countingSimTop  IS
-	GENERIC (n : INTEGER := 10;		 --; 	-- bit-widths of memory pointer, counter and etc,,, 
+ENTITY countingSim  IS
+	GENERIC (n : INTEGER := 10;		 --; 	-- bit-widths of memory pointer, counter and etc,,,
 			 d : INTEGER := 10;		 	 	-- number of confComp module
-			 z		 : INTEGER := 0;		 -- zeropadding to 2** for RSA 
+			 z		 : INTEGER := 0;		 -- zeropadding to 2** for RSA
 			 classNumber : INTEGER := 10; 		---- class number --- for memory image
 			 logInNum : INTEGER := 3	);   -- MuxCell, ceilingLOG2(#popCounters)
 	PORT (
-		clk, rst, run  	: IN STD_LOGIC;	
-		hv        		: IN  STD_LOGIC_VECTOR(d -1 DOWNTO 0);	
-		done       		: OUT  STD_LOGIC;	
-		pointer		 	: OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0);	
-		dout	 		: OUT  STD_LOGIC_VECTOR(classNumber*(n+logInNum)-1 DOWNTO 0)  	
-	);
-END ENTITY countingSimTop ;
-
-ARCHITECTURE behavioral OF countingSimTop IS
-
-component countingSim  IS
-	GENERIC (n : INTEGER := 10;		 --; 	-- bit-widths of memory pointer, counter and etc,,, 
-			 d : INTEGER := 10;		 	 	-- number of confComp module
-			 z		 : INTEGER := 0;		 -- zeropadding to 2** for RSA 
-			 classNumber : INTEGER := 10; 		---- class number --- for memory image
-			 logInNum : INTEGER := 3	);   -- MuxCell, ceilingLOG2(#popCounters OR d)
-	PORT (
-		clk, rst, run, done  	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
-		reg1Update, reg1rst, reg2Update, reg2rst   	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
+		clk, rst, run, done  	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ----
+		reg1Update, reg1rst, reg2Update, reg2rst   	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ----
 		muxSel   	 	: IN  STD_LOGIC_VECTOR (logInNum DOWNTO 0);
 		hv        		: IN  STD_LOGIC_VECTOR(d -1 DOWNTO 0);
 		CHV        		: IN  STD_LOGIC_VECTOR(d -1 DOWNTO 0);
-		pointer		 	: IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);	
-		dout	 		: OUT  STD_LOGIC_VECTOR(n+logInNum-1 DOWNTO 0)  	
+		pointer		 	: IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+		dout	 		: OUT  STD_LOGIC_VECTOR(n+logInNum-1 DOWNTO 0)
 	);
-end component;
+END ENTITY countingSim ;
 
-component SeqAdderCtrl IS
-	GENERIC (ceilingLogPop : INTEGER := 3;   -- ceilingLOG2(#popCounters)
-			nPop : INTEGER := 8 );			-- #popCounters
+ARCHITECTURE behavioral OF countingSim IS
+
+component fullconfComp  IS
+	GENERIC (n : INTEGER := 10;		 --; 	-- bit-widths of memory pointer, counter and etc,,,
+			 classNumber : INTEGER := 10; 		---- class number --- for memory image
+			 classPortion : INTEGER := 10 ); 		---- portion of class memory --- for memory image
 	PORT (
-		clk, rst 				: IN STD_LOGIC;
-		run		 				: IN STD_LOGIC;
-		reg1Update, reg1rst 	: OUT STD_LOGIC;
-		reg2Update, reg2rst 	: OUT STD_LOGIC;
-		muxSel 					: OUT STD_LOGIC_VECTOR(ceilingLogPop DOWNTO 0)
+		clk, rst, run, done  	: IN STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ----
+		hv        		: IN  STD_LOGIC;
+		Chv_input       		: IN  STD_LOGIC; -- _vector ((2**n)-1 DOWNTO 0);
+		pointer		 	: IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+		sim		 		: OUT  STD_LOGIC_VECTOR(n-1 DOWNTO 0)
 	);
 end component;
- 
-component countingSimCtrl IS
-	GENERIC (n : INTEGER := 10 ); --- bit pointer to memory
+
+component RSA IS
+	GENERIC (inLen : INTEGER := 8;		 -- bit width out popCounters
+			 d		 : INTEGER := 8;		 -- d,,, #popCounters
+			 z		 : INTEGER := 0;		 -- zeropadding to 2**
+			 logInNum : INTEGER := 3	);   -- MuxCell, ceilingLOG2(#popCounters)
 	PORT (
-		clk, rst 				: IN STD_LOGIC;
-		run		 				: IN STD_LOGIC;
-		runOut, done 			: OUT STD_LOGIC;
-		pointer 				: OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0) --- As of now only support up to 16 classes so 4'bits 
+		clk, rst, reg1Update, reg1rst, reg2Update, reg2rst 	: IN STD_LOGIC;
+		muxSel    : IN  STD_LOGIC_VECTOR (logInNum DOWNTO 0);
+		A         : IN  STD_LOGIC_VECTOR (((d)*inLen)- 1 DOWNTO 0);  -- cascade with enough 0 as input or inner signal! lets check!
+		B         : OUT  STD_LOGIC_VECTOR (inLen + logInNum - 1 DOWNTO 0)
 	);
 end component;
 
---new memory signals
-type CHV_memory is array (classNumber-1 downto 0) of std_logic_vector((2**(n))*d-1 downto 0);
-type CHV_memory_tosim is array (classNumber-1 downto 0) of std_logic_vector(d-1 downto 0);
-signal CHV_TO_OUT : CHV_memory_tosim;
-
-signal CHV : CHV_memory;
-file CHV_file : text open read_mode is "CHV_img.mif"; -- Specify your file name
-
-SIGNAL dones, runOut	: STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
-SIGNAL reg1Update, reg1rst, reg2Update, reg2rst	: STD_LOGIC;				---- run shuld be always '1' during calculation --- ctrl ---- 
-SIGNAL muxSel	:  STD_LOGIC_VECTOR (logInNum DOWNTO 0);
-SIGNAL point	: STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-
-attribute MARK_DEBUG : string;
-attribute MARK_DEBUG of CHV : signal is "TRUE";
-attribute MARK_DEBUG of CHV_TO_OUT : signal is "TRUE";
+SIGNAL sim : STD_LOGIC_VECTOR (((d)*n)- 1 DOWNTO 0);
 
 begin
-
-    process
-	variable mif_line : line;
-	variable temp_bv : bit_vector((2**(n))*d-1 downto 0); -- Temporary buffer for each line
-    begin
-        -- Loop through each line of the file
-        for i in 0 to classNumber-1 loop
-            if not endfile(CHV_file) then
-                -- Read one line from the file
-                readline(CHV_file, mif_line);
-                -- Read the binary data into the temporary bit_vector
-                read(mif_line, temp_bv);
-                -- Convert the bit_vector to std_logic_vector and store it in the memory signal
-                CHV(i) <= to_stdlogicvector(temp_bv);
-            else
-                -- Handle end of file if fewer lines exist than expected
-                CHV(i) <= (others => '0'); -- Optional: Initialize remaining entries to 0
-            end if;
-        end loop;
-        wait; -- Stop the process after reading the file
-    end process; 
-
-    concatECC: FOR I IN classNumber-1 DOWNTO 0 GENERATE
-        classesECC: FOR k IN d-1 DOWNTO 0 GENERATE
-            CHV_TO_OUT(I)(k) <= CHV(I)(to_integer(unsigned(point)) + (2**(n)) * k);
-        END GENERATE classesECC;
-    END GENERATE concatECC;
-
-	AdderCtrl : SeqAdderCtrl
-	GENERIC MAP(logInNum, 
-			d )
-	PORT MAP(
-            clk, rst,
-            dones,
-            reg1Update, reg1rst,
-            reg2Update, reg2rst,
-            muxSel
-        );	
-    
-	countSimArr: FOR I IN classNumber-1 DOWNTO 0 GENERATE
-		comp : countingSim 
-		GENERIC MAP(n ,d, z, I, logInNum)
+--chv_to_fullconf <= zesros & chv;
+	compArr: FOR I IN d DOWNTO 1 GENERATE
+		comp : fullconfComp
+		GENERIC MAP(n, classNumber, I-1) ------- bayad ye array begiram!
 		PORT MAP(
-			clk, rst, runOut, dones, 
-			reg1Update, reg1rst, reg2Update, reg2rst,
-			muxSel,
-			hv, CHV_TO_OUT(I),
-			point,
-			dout(((I+1)*(n+logInNum))- 1 DOWNTO ((I)*(n+logInNum)))
+			clk, rst, run, done, hv(I-1),chv(I-1),  --,chv((2**n)*(I)-1 downto (2**n)*(I-1))
+			pointer,
+			sim	((I*n)- 1 DOWNTO ((I-1)*n))
 		);
-	END GENERATE countSimArr;
-		
-	CompCtrl : countingSimCtrl
-	GENERIC MAP(n)
-	PORT MAp(
-		clk, rst,
-		run,
-		runOut, dones, 
-		point
+	end generate compArr;
+
+	seqAdd : RSA
+	GENERIC MAP(n, d, z, logInNum)   -- MuxCell, ceilingLOG2(#popCounters)
+	PORT MAP(
+		clk, rst, reg1Update, reg1rst, reg2Update, reg2rst,
+		muxSel,
+		sim,
+		dout
 	);
 	
-	pointer <= point;
-	done <= reg2Update;
-	
+
 end architecture;
-
-
-
