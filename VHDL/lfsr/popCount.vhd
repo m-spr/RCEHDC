c54b84d28778074bb8831c852c5219b98b4a3f0e
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-17 18:34:40 +0100

    Update popCount.vhd

--- VHDL/lfsr/popCount.vhd
+++ VHDL/lfsr/popCount.vhd
@@ -25,29 +25,33 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY popCount IS
-	GENERIC (lenPop : INTEGER := 8);   -- bit width out popCounters --- LOG2(#feature)
-	PORT (
-		clk , rst 	: IN STD_LOGIC;
-		en		 	: IN STD_LOGIC;
-		dout        : OUT  STD_LOGIC_VECTOR (lenPop-1 DOWNTO 0)
-	);
+    GENERIC (
+        lenPop : INTEGER := 8  -- Bit width of output popCounters (LOG2(#feature))
+    );
+    PORT (
+        clk  : IN  STD_LOGIC;
+        rst  : IN  STD_LOGIC;
+        en   : IN  STD_LOGIC;
+        dout : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+    );
 END ENTITY popCount;
 
 ARCHITECTURE behavioral OF popCount IS
-SIGNAL popOut : STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
-	
+    SIGNAL popOut : STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0) := (OTHERS => '0');  -- Initialize to zero
 BEGIN
 
-	PROCESS(clk)
-		BEGIN 
-		    IF (clk ='1' and clk'event)THEN
-			    IF(rst = '1')THEN
-				   popOut <= (OTHERS=>'0');
-				ELSIF (en ='1') THEN 
-				   popOut <= STD_LOGIC_VECTOR (UNSIGNED(popOut) + 1);
-				END IF;
-			END IF;
-	END PROCESS;
+    PROCESS(clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF rst = '1' THEN
+                popOut <= (OTHERS => '0');  -- Reset to zero
+            ELSIF en = '1' THEN
+                popOut <= STD_LOGIC_VECTOR(UNSIGNED(popOut) + 1);  -- Increment counter
+            END IF;
+        END IF;
+    END PROCESS;
 
-	dout <= popOut;
+    -- Assign output
+    dout <= popOut;
+
 END ARCHITECTURE behavioral;
