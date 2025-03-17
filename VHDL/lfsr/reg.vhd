560a108e4612c415a6a88e884b91584a4318f68d
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-17 18:31:20 +0100

    Update reg.vhd

--- VHDL/lfsr/reg.vhd
+++ VHDL/lfsr/reg.vhd
@@ -24,6 +24,40 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
+ENTITY reg IS
+    GENERIC (
+        lenPop : INTEGER := 8  -- Bit width out popCounters
+    );
+    PORT (
+        clk       : IN  STD_LOGIC;
+        regUpdate : IN  STD_LOGIC;
+        regrst    : IN  STD_LOGIC;
+        din       : IN  STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0);
+        dout      : OUT STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0)
+    );
+END ENTITY reg;
+
+ARCHITECTURE behavioral OF reg IS
+    SIGNAL regOut : STD_LOGIC_VECTOR(lenPop - 1 DOWNTO 0) := (OTHERS => '0'); -- Initialize with zeros
+BEGIN
+    PROCESS(clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF regrst = '1' THEN
+                regOut <= (OTHERS => '0');  -- Reset value: all zeros
+            ELSIF regUpdate = '1' THEN
+                regOut <= din;
+            END IF;
+        END IF;
+    END PROCESS;
+
+    dout <= regOut;
+END ARCHITECTURE behavioral;
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
 ENTITY reg1 IS
     GENERIC (
         lenPop : INTEGER := 8  -- Bit width out popCounters
