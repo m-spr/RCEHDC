529e6e738e4c64cb57a8daca132d449dc778c0e1
m-spr <53905453+m-spr@users.noreply.github.com>
2025-03-26 15:05:08 +0100

    Update encoder.vhd

--- VHDL/lfsr/encoder.vhd
+++ VHDL/lfsr/encoder.vhd
@@ -25,92 +25,121 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY encoder IS
-	GENERIC (d :  INTEGER := 500; 	-- dimension size 
-			lgf  : INTEGER := 10;		 -- bit width out popCounters --- LOG2(#feature) --- nabayad in bashe!?! whats wrong with me?
-			featureSize		: INTEGER := 700
-			);	 
-	PORT (
-		clk, rst	: IN STD_LOGIC; 	-- run should be '1' for 2 clk cycle!!!!! badan behehesh fekr mikonam!!
-		run			: IN  STD_LOGIC;
-		din			: IN  STD_LOGIC_VECTOR (d-1 DOWNTO 0);
-		BV			: IN  STD_LOGIC_VECTOR (d-1 DOWNTO 0);
-		rundegi		: OUT STD_LOGIC;
-		done, ready_M		: OUT  STD_LOGIC;
-		dout		: OUT  STD_LOGIC_VECTOR (d-1 DOWNTO 0)
-	);
+    GENERIC (
+        d           : INTEGER := 500;     -- Dimension size
+        lgf         : INTEGER := 10;      -- Bit width of popCounters output (log2(featureSize))
+        featureSize : INTEGER := 700      -- Number of features
+    );
+    PORT (
+        clk         : IN  STD_LOGIC;      -- Clock signal
+        rst         : IN  STD_LOGIC;      -- Reset signal
+        run         : IN  STD_LOGIC;      -- Run should be '1' for 2 clk cycles
+        din         : IN  STD_LOGIC_VECTOR (d - 1 DOWNTO 0); -- Input data
+        BV          : IN  STD_LOGIC_VECTOR (d - 1 DOWNTO 0); -- Bit vector for feature selection
+        rundegi     : OUT STD_LOGIC;      -- Signal indicating end of round (?)
+        done        : OUT STD_LOGIC;      -- Done signal
+        ready_M     : OUT STD_LOGIC;      -- Ready signal
+        dout        : OUT STD_LOGIC_VECTOR (d - 1 DOWNTO 0) -- Output data
+    );
 END ENTITY encoder;
 
 ARCHITECTURE behavioral OF encoder IS
 
-component reg IS
-	GENERIC (lenPop : INTEGER := 8);   -- bit width out popCounters
-	PORT (
-		clk 		: IN STD_LOGIC;
-		regUpdate, regrst 	: IN STD_LOGIC;
-		din         : IN  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
-		dout        : OUT  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
-	);
-END component;
+    COMPONENT reg IS
+        GENERIC (
+            lenPop : INTEGER := 8         -- Bit width of reg output
+        );
+        PORT (
+            clk      : IN  STD_LOGIC;
+            regUpdate: IN  STD_LOGIC;
+            regrst   : IN  STD_LOGIC;
+            din      : IN  STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0);
+            dout     : OUT STD_LOGIC_VECTOR (lenPop - 1 DOWNTO 0)
+        );
+    END COMPONENT;
 
-COMPONENT XoringInputPop IS
-	GENERIC ( n  : INTEGER := 8		 -- number of loop
-			 );	 
-	PORT (
-		clk, rst, update	: IN STD_LOGIC; 	--- bit width out popCounters --- LOG2(#feature)
-		done	: IN  STD_LOGIC;
-		din		: IN  STD_LOGIC;
-		BV		: IN  STD_LOGIC;
-		dout	: OUT  STD_LOGIC_VECTOR (n-1 DOWNTO 0)
-	);
-END COMPONENT;
+    COMPONENT XoringInputPop IS
+        GENERIC (
+            n : INTEGER := 8              -- Number of bits for popcount output
+        );
+        PORT (
+            clk     : IN  STD_LOGIC;
+            rst     : IN  STD_LOGIC;
+            update  : IN  STD_LOGIC;
+            done    : IN  STD_LOGIC;
+            din     : IN  STD_LOGIC;
+            BV      : IN  STD_LOGIC;
+            dout    : OUT STD_LOGIC_VECTOR (n - 1 DOWNTO 0)
+        );
+    END COMPONENT;
 
-COMPONENT  XoringPopCtrl IS
-	GENERIC (n  : INTEGER := 10 ;			----bit width out popCounters --- ceiling log2(#feature)
-			featureSize		: INTEGER := 700  );	---- NUmber of loops for popcounts 
-	PORT (
-		clk, rst 				: IN STD_LOGIC;
-		run		 				: IN STD_LOGIC;
-		rundegi, update, doneI, doneII, ready_M		    : OUT STD_LOGIC
-	);
-END COMPONENT;
+    COMPONENT XoringPopCtrl IS
+        GENERIC (
+            n           : INTEGER := 10;   -- Bit width for popCounter (log2(featureSize))
+            featureSize : INTEGER := 700   -- Number of loops
+        );
+        PORT (
+            clk        : IN  STD_LOGIC;
+            rst        : IN  STD_LOGIC;
+            run        : IN  STD_LOGIC;
+            rundegi    : OUT STD_LOGIC;
+            update     : OUT STD_LOGIC;
+            doneI      : OUT STD_LOGIC;
+            doneII     : OUT STD_LOGIC;
+            ready_M    : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
-signal update, doneI : STD_LOGIC;
-CONSTANT test : STD_LOGIC_VECTOR (lgf-1 downto 0) := STD_LOGIC_VECTOR(to_UNSIGNED(featureSize/2 , lgf));
-signal douttest	:  STD_LOGIC_VECTOR (d-1 DOWNTO 0);
-signal doutXOR : STD_LOGIC_VECTOR ((d*lgf)-1 downto 0);
-signal querycheck : STD_LOGIC_VECTOR ((d)-1 downto 0);
-signal testID : STD_LOGIC_VECTOR (1000-1 DOWNTO 0) := (others=>'0');
-signal testBV : STD_LOGIC_VECTOR (1000-1 DOWNTO 0) := (others=>'0');
-SIGNAL IDeq, BVeq, eq : STD_LOGIC;
-
--- attribute MARK_DEBUG : string;
--- attribute MARK_DEBUG of doutXOR : signal is "TRUE";
-
+    SIGNAL update       : STD_LOGIC;
+    SIGNAL doneI        : STD_LOGIC;
+    CONSTANT test       : STD_LOGIC_VECTOR (lgf - 1 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(featureSize / 2, lgf));
+    SIGNAL douttest     : STD_LOGIC_VECTOR (d - 1 DOWNTO 0);
+    SIGNAL doutXOR      : STD_LOGIC_VECTOR ((d * lgf) - 1 DOWNTO 0);
+    SIGNAL querycheck   : STD_LOGIC_VECTOR (d - 1 DOWNTO 0);
+    SIGNAL testID       : STD_LOGIC_VECTOR (999 DOWNTO 0) := (others => '0');
+    SIGNAL testBV       : STD_LOGIC_VECTOR (999 DOWNTO 0) := (others => '0');
+    SIGNAL IDeq, BVeq, eq : STD_LOGIC;
 
 BEGIN
 
+    -- Instantiate d parallel popcount units
+    popCounters : FOR I IN 0 TO d - 1 GENERATE
+        pop: XoringInputPop 
+            GENERIC MAP (
+                n => lgf
+            )
+            PORT MAP (
+                clk     => clk,
+                rst     => rst,
+                update  => update,
+                done    => doneI,
+                din     => din(I),
+                BV      => BV(I),
+                dout    => doutXOR((I + 1) * lgf - 1 DOWNTO I * lgf)
+            );
+    END GENERATE popCounters;
 
-	popCounters : FOR I IN 0 TO d-1 GENERATE
-		pop: XoringInputPop 
-			GENERIC MAP(lgf)
-			PORT MAP(
-				clk , rst, update, doneI,
-				din(I), BV(I),
-				doutXOR((I+1)*lgf-1 DOWNTO (I)*lgf)
-			);
-	END GENERATE popCounters;	
+    -- Instantiate control logic
+    ctrl : XoringPopCtrl
+        GENERIC MAP (
+            n           => lgf,
+            featureSize => featureSize
+        )
+        PORT MAP (
+            clk      => clk,
+            rst      => rst,
+            run      => run,
+            rundegi  => rundegi,
+            update   => update,
+            doneI    => doneI,
+            doneII   => done,
+            ready_M  => ready_M
+        );
 
-	ctrl :  XoringPopCtrl 
-	GENERIC MAP(lgf, featureSize)
-	PORT MAP(
-		clk, rst, run,rundegi,
-		update, doneI, done, ready_M
-		);
-	
-	doutGen : FOR I IN 0 TO d-1 GENERATE
-			dout (I) <= '1' WHEN ( doutXOR((I+1)*lgf-1 DOWNTO (I)*lgf) > test) ELSE '0';
-	END GENERATE doutGen;
-	
-
+    -- Generate output decision: thresholding each lgf-bit counter against "test"
+    doutGen : FOR I IN 0 TO d - 1 GENERATE
+        dout(I) <= '1' WHEN doutXOR((I + 1) * lgf - 1 DOWNTO I * lgf) > test ELSE '0';
+    END GENERATE doutGen;
 
 END ARCHITECTURE behavioral;
+
